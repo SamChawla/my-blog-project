058067cae429139809348eea4c37ad720ca0c1e2
Sumit S Chawla <sam.chawla26@gmailc.om>
2021-04-21 10:26:18 +0530

    added Post model and migration

--- my_site/blogs/models.py
+++ my_site/blogs/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ("draft", "Draft"),
+        ("published", "Published"),
+    )
+    title = models.CharField(max_length=250)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
+    content = models.TextField()
+    published_at = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="draft")
+
+    class Meta:
+        ordering = ("-published_at",)
+
+    def __str__(self):
+        return self.title
