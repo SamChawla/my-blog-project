69e39dd09da502f86f4691ffebf777b56a8f9c48
Sumit S Chawla <sam.chawla26@gmail.com>
2021-05-01 10:00:49 +0530

    login-logout system added (#10)
    Co-authored-by: Sumit S Chawla <sam.chawla26@gmailc.om>

--- my_site/accounts/urls.py
+++ my_site/accounts/urls.py
@@ -1,6 +1,10 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path('register', views.register, name="register-user"),
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
 ]
