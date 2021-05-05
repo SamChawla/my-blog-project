ab187831d8846e9eec93c9f1f8d12c69d788c1eb
Sumit S Chawla <sam.chawla26@gmailc.om>
2021-05-05 11:01:09 +0530

    CBV added for home and detail page

--- my_site/blogs/urls.py
+++ my_site/blogs/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="blog-home"),
+    # path("", views.home, name="blog-home"),
+    path("posts/", views.post_list_view, name="blog-home"),
+    path('posts/<int:pk>/', views.post_detail_view, name='post-detail'),
     path("about/", views.about, name="blog-about"),
 ]
--- my_site/blogs/views.py
+++ my_site/blogs/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 from datetime import datetime
+
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
 from .models import Post
 
 
@@ -16,3 +19,29 @@ def home(request):
 
 def about(request):
     return render(request, template_name="blogs/about.html")
+
+
+class PostListView(ListView):
+    model = Post
+    queryset = Post.published.all()
+    template_name = 'blogs/home.html'
+    context_object_name = 'posts'
+    ordering = ['-published_at']
+
+    # def get_queryset(self):
+    #     qs = super().get_queryset()
+    #     return qs.filter(status='published') 
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context_data = super().get_context_data()
+        context_data['title'] = "BLOG HOME PAGE"
+        return context_data
+
+post_list_view = PostListView.as_view()
+
+
+class PostDetailView(DetailView):
+    model = Post
+    # pk_url_kwarg = 'id'
+
+post_detail_view = PostDetailView.as_view()
