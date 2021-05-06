fc1ba200a7fa1860b2bc2a55023b008eed280a55
Sumit S Chawla <sam.chawla26@gmail.com>
2021-05-06 11:00:21 +0530

    CRUD for POSTS (#14)
    Co-authored-by: Sumit S Chawla <sam.chawla26@gmailc.om>

--- my_site/blogs/models.py
+++ my_site/blogs/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
 
@@ -27,6 +28,10 @@ class Post(models.Model):
 
     class Meta:
         ordering = ("-published_at",)
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
+
 
     def __str__(self):
         return f"{self.id} - {self.title}"
